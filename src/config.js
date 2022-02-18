072e0a0fc3cfc84d9a3d2b8b28cc3804f379ac5f
bruno <n3586@hotmail.com>
2022-02-18 14:06:33 -0300

    deploy

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-// export const base_url = "http://localhost:3001";
-export const base_url = "https://ji-react.vercel.app:3001";
+export const base_url = "http://localhost:3001";
+// export const base_url = "https://ji-react.vercel.app:3001";

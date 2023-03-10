78b875a6eee568dbb22c8de44176d39746940869
Bruno Esteves <n3586@hotmail.com>
2023-03-10 11:34:53 -0300

    render8

--- backend/server/index.ts
+++ backend/server/index.ts
@@ -3,10 +3,10 @@ import http from "http";
 import { json } from "body-parser";
 import express from "express";
 import { ApolloServer } from "@apollo/server";
-// import { expressMiddleware } from "@apollo/server/express4";
+import { expressMiddleware } from "@apollo/server/express4";
 
 import { buildSchema } from "type-graphql";
-import { graphqlHTTP } from "express-graphql";
+// import { graphqlHTTP } from "express-graphql";
 import cors from "cors";
 
 import { CategoriesResolver } from "./Resvolvers/categories-resolvers";
@@ -27,13 +27,13 @@ const main = async () => {
   });
   await server.start();
 
-  app.use(
-    "/graphql",
-    graphqlHTTP({
-      schema,
-      graphiql: true,
-    })
-  );
+  // app.use(
+  //   "/graphql",
+  //   graphqlHTTP({
+  //     schema,
+  //     graphiql: true,
+  //   })
+  // );
 
   app.use(
     "/graphql",
@@ -44,8 +44,8 @@ const main = async () => {
       ],
     }),
 
-    json()
-    // expressMiddleware(server)
+    json(),
+    expressMiddleware(server)
   );
 
   await new Promise<void>((resolve) => {

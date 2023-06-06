d22ea2cda5177a40252a96aa43a8e9009ca952e0
Bruno <n3586@hotmail.com>
2023-06-06 16:58:12 -0300

    change source

--- frontend/src/Graphql/GraphqlSetup.ts
+++ frontend/src/Graphql/GraphqlSetup.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 import { createUploadLink } from 'apollo-upload-client';
 
 const link = createUploadLink({
-  uri: process.env.REACT_APP_PUBLIC_URL2
+  uri: process.env.REACT_APP_PUBLIC_URL
 });
 
 export const client = new ApolloClient({
